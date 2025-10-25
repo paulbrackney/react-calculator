a4c5ac492ff6037edb432825f8f3d843a94745be
Paul Brackney <paulbrack2000@gmail.com>
2025-10-25 13:41:09 -0500

    Remove base path and vercel.json

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/react-calculator/',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-  }
 })
