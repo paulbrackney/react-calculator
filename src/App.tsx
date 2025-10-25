eaf71b3892261363e3e60fa8dec691fb3b257aab
Paul Brackney <paulbrack2000@gmail.com>
2025-10-25 12:09:46 -0500

    Pushing initial calculator code to git

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,163 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [input, setInput] = useState('0');
+  const [isResult, setIsResult] = useState(false);
+
+  function subtract() {
+    let result = 0;
+    let tokens = input.split('-');
+    tokens.forEach((token, idxToken) => {
+      token.trim();
+      if (idxToken === 0) {
+        result += Number(token);
+      } else {
+        result -= Number(token);
+      }
+    })
+    return result;
+  }
+
+  function add() {
+    let result = 0;
+    let tokens = input.split('+');
+    tokens.forEach((token) => {
+      token.trim();
+      result += Number(token);
+    })
+    return result;
+  }
+
+  function multiply() {
+    let result = 0;
+    let tokens = input.split('\u00d7');
+    tokens.forEach((token, idxToken) => {
+      token.trim();
+      if (idxToken === 0) {
+        result = Number(token);
+      } else {
+        result *= Number(token);
+      }
+    })
+    return result;
+  }
+
+  function divide() {
+    let result = 0;
+    const temp = input;
+    let tokens = input.split('\u00f7');
+    tokens.forEach((token, idxToken) => {
+      token.trim();
+      if (idxToken === 0) {
+        result = Number(token);
+      } else {
+        result /= Number(token);
+      }
+    })
+    return result;
+  }
+
+  function calculate() {
+    let result = 0;
+
+    if (input.includes('-')) result = subtract();
+    if (input.includes('+')) result = add();
+    if (input.includes('\u00d7')) result = multiply();
+    if (input.includes('\u00f7')) result = divide();
+    
+    setInput(result.toString());
+    setIsResult(true);
+  }
+
+  function isOperator(string: string): boolean {
+    return string === '+' ||
+           string === '-' || 
+           string === '\u00d7' ||
+           string === '\u00f7';
+  }
+
+  function onClick(id: string): void {
+    let temp = input;
+    if (isResult || temp === '0') {
+      if (!isOperator(id)) temp = '';
+      setIsResult(false);
+    }
+    if (id === '=') {
+      calculate();
+    } else if (isOperator(id)) {
+      setInput(temp + ` ${id} `)
+    } else {
+      setInput(temp + id);
+    }
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="container">
+        <div className="input">
+          {input}
+        </div>
+        <div className="row">
+          <div id="7" className="key number" onClick={() => onClick("7")}>
+            7
+          </div>
+          <div id="8" className="key number" onClick={() => onClick("8")}>
+            8
+          </div>
+          <div id="9" className="key number" onClick={() => onClick("9")}>
+            9
+          </div>
+          <div id="&times" className="key operator" onClick={() => onClick("\u00d7")}>
+            &times;
+          </div>
+        </div>
+        <div className="row">
+          <div id="4" className="key number" onClick={() => onClick("4")}>
+            4
+          </div>
+          <div id="5" className="key number" onClick={() => onClick("5")}>
+            5
+          </div>
+          <div id="6" className="key number" onClick={() => onClick("6")}>
+            6
+          </div>
+          <div id="&minus" className="key operator" onClick={() => onClick("-")}>
+          &minus;
+          </div>
+        </div>
+        <div className="row">
+          <div id="1" className="key number" onClick={() => onClick("1")}>
+            1
+          </div>
+          <div id="2" className="key number" onClick={() => onClick("2")}>
+            2
+          </div>
+          <div id="3" className="key number" onClick={() => onClick("3")}>
+            3
+          </div>
+          <div id="+" className="key operator" onClick={() => onClick("+")}>
+            +
+          </div>
+        </div>
+        <div className="row">
+          <div id="0" className="key number" onClick={() => onClick("0")}>
+            0
+          </div>
+          <div id="." className="key number" onClick={() => onClick(".")}>
+            .
+          </div>
+          <div id="&divide" className="key operator" onClick={() => onClick("\u00f7")}>
+            &divide;
+          </div>
+          <div id="=" className="key operator" onClick={() => onClick("=")}>
+            = 
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
 
 export default App
+
